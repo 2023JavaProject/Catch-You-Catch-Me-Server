d2b97ff59fad1de75619218286a5be3b29f28d51
haeunNoh06 <okhagrace06@naver.com>
2023-11-09 20:04:49 +0900

    :sparkles: [feat] 채팅 server, client 통신 구현 완료

---
+++ src/main/java/org/example/ChatServer.java
@@ -0,0 +1,88 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ChatServer {
+    private static final int PORT = 8090;
+    private static Set<PrintWriter> clientWriters = new HashSet<>();
+    private static ExecutorService clientHandlerThreadPool = Executors.newCachedThreadPool();
+    private Socket serverSocket;
+    private static boolean isOn = false;
+    public ChatServer() {
+        System.out.println("채팅 서버 시작...");
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+//            serverSocket.setReuseAddress(true);
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                ClientHandler clientHandler = new ClientHandler(clientSocket);
+                clientHandlerThreadPool.execute(clientHandler);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static class ClientHandler implements Runnable {
+        private Socket socket;
+        private PrintWriter out;
+
+        public ClientHandler(Socket socket) {
+
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                out = new PrintWriter(socket.getOutputStream(), true);
+
+                synchronized (clientWriters) {
+                    clientWriters.add(out);
+                }
+
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String message;
+                while ((message = in.readLine()) != null) {
+                    System.out.println("수신: " + message);
+                    broadcastMessage(message);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                synchronized (clientWriters) {
+                    clientWriters.remove(out);
+                }
+            }
+        }
+
+        private void broadcastMessage(String message) {
+            synchronized (clientWriters) {
+                for (PrintWriter writer : clientWriters) {
+                    writer.println("수신: " + message);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new ChatServer();
+    }
+}
