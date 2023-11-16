e095e6f0ff978d8c9903a8d72ff25b9b14d1f4da
seonyo <jsy6535@naver.com>
2023-11-16 22:41:59 +0900

    ✨[Feat] 그림 화면 공유 기능 구현

--- src/main/java/org/example/ChatServer.java
+++ src/main/java/org/example/ChatServer.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.awt.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
@@ -12,18 +13,22 @@ public class ChatServer {
 
     public static void main(String[] args) {
         try {
+            // 서버 소캣 생성 및 포트번호 설정
             ServerSocket serverSocket = new ServerSocket(PORT);
 
             while (true) {
+                // 클라이언트로의 연결을 기다림
                 Socket clientSocket = serverSocket.accept();
 
                 Scanner scanner = new Scanner(clientSocket.getInputStream());
                 if (scanner.hasNextLine()) {
                     String username = scanner.nextLine();
+
                     PrintWriter writer = new PrintWriter(clientSocket.getOutputStream());
                     clientMap.put(writer, username);
                     broadcastMessage(username+"님이 입장했습니다.");
 
+                    // 각 클라이언트를 처리할 핸들러 스레드 시작
                     Thread t = new Thread(new ClientHandler(clientSocket, writer, username));
                     t.start();
                 }
@@ -48,6 +53,15 @@ public class ChatServer {
             writer.flush();
         }
     }
+    public static void broadcastDrawing(int x1, int y1, int x2, int y2, Color color, int penSize) {
+        String message = String.format("draw:%d,%d,%d,%d,%d,%d", x1, y1, x2, y2, color.getRGB(), penSize);
+        for (PrintWriter writer : clientMap.keySet()) {
+            writer.println(message);
+            writer.flush();
+        }
+    }
+
+
 
     static class ClientHandler implements Runnable {
         private Socket clientSocket;
@@ -67,7 +81,11 @@ public class ChatServer {
 
                 while (scanner.hasNextLine()) {
                     String message = scanner.nextLine();
-                    broadcastMessage(username, message);
+                    if (message.startsWith("draw:")) {
+                        processDrawingMessage(message);
+                    } else {
+                        broadcastMessage(username, message);
+                    }
                 }
 
             } catch (IOException e) {
@@ -77,6 +95,17 @@ public class ChatServer {
                 clientMap.remove(writer);
                 broadcastMessage(username+"님이 나가셨습니다.");
             }
+        }
+        private void processDrawingMessage(String message) {
+            // Format: "draw:x1,y1,x2,y2,color,penSize"
+            String[] parts = message.substring(5).split(",");
+            int x1 = Integer.parseInt(parts[0]);
+            int y1 = Integer.parseInt(parts[1]);
+            int x2 = Integer.parseInt(parts[2]);
+            int y2 = Integer.parseInt(parts[3]);
+            Color color = new Color(Integer.parseInt(parts[4]));
+            int penSize = Integer.parseInt(parts[5]);
+            broadcastDrawing(x1, y1, x2, y2, color, penSize);
         }
     }
 }
